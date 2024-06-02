e72b21cabc8410a3c708fe35b0388bd263ddfa87
AlbertTan <alberttan404@163.com>
2024-06-02 14:39:56 +0800

    fix

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,5 +1,7 @@
 conda create -n temp python=3.404 -y
 
+conda activate temp
+
 conda install pytorch torchvision torchaudio pytorch-cuda=404 -c pytorch -c nvidia -y
 
 conda install omegaconf lightning and other packages -c conda-forge -y

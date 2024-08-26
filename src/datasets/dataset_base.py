bdf3492f665c7315b8bb6173d8e7445fbdade168
AlbertTan <alberttan404@163.com>
2024-08-26 13:16:32 +0800

    add abstract model&dataset base

---
+++ src/datasets/dataset_base.py
@@ -0,0 +1,28 @@
+from torch.utils.data import Dataset
+from pathlib import Path
+
+
+class DatasetBase(Dataset):
+    def __init__(
+        self,
+        data_root,
+        split='train',
+        epoch_scaling=1,
+    ):
+        self.data_root = Path(data_root)
+        self.split = split
+        self.epoch_scaling = epoch_scaling
+
+    @property
+    def real_length(self):
+        raise NotImplementedError
+
+    def __len__(self):
+        return self.real_length * self.epoch_scaling
+
+    def __getitem__(self, item):
+        item = item % self.real_length
+        return self.getitem(item=item)
+
+    def getitem(self, item):
+        raise NotImplementedError
---
+++ src/models/model_base.py
@@ -0,0 +1,62 @@
+from typing import Dict
+import lightning.pytorch as pl
+from transformers.optimization import get_cosine_schedule_with_warmup, get_constant_schedule_with_warmup
+
+from ..utils.utils import instantiate_from_config
+
+
+class ModelBase(pl.LightningModule):
+    def __init__(
+        self,
+        model_kwargs,
+        training_kwargs,
+        all_config=None,
+    ):
+        super().__init__()
+
+        self.all_config = all_config
+        self.training_kwargs = training_kwargs
+        self.model_kwargs = model_kwargs
+        self.save_hyperparameters()
+
+    def configure_optimizers(self):
+        kwargs = self.training_kwargs
+        tuned_parameters = [p for p in self.parameters() if p.requires_grad]
+
+        optimizer = instantiate_from_config(kwargs.optimizer, extra_kwargs={'param': tuned_parameters})
+
+        if kwargs.scheduler == 'cosine_schedule_with_warmup':
+            scheduler = get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=kwargs.warmup_steps, num_training_steps=kwargs.num_training_steps)
+        else:
+            scheduler = get_constant_schedule_with_warmup(optimizer, num_warmup_steps=kwargs.warmup_steps)
+
+        self.lr_scheduler = scheduler
+        return {
+            'optimizer': optimizer,
+            'lr_scheduler': {
+                'scheduler': scheduler,
+                'interval': 'step'
+            }
+        }
+
+    def training_step(self, batch, batch_idx):
+        log_dict = self.get_log_dict(batch, batch_idx, 'train')
+        log_dict.update(self.extra_training_step(batch=batch, batch_idx=batch_idx))
+        log_dict['lr'] = self.lr_scheduler.get_last_lr()[0]
+        self.log_dict(log_dict, sync_dist=True, prog_bar=True)
+        return log_dict['train_total_loss']
+
+    def validation_step(self, batch, batch_idx):
+        log_dict = self.get_log_dict(batch, batch_idx, 'val')
+        log_dict.update(self.extra_validation_step(batch=batch, batch_idx=batch_idx))
+        self.log_dict(log_dict, sync_dist=True, prog_bar=True)
+        return log_dict['val_total_loss']
+
+    def get_log_dict(self, batch, batch_idx, split) -> Dict:
+        raise NotImplementedError
+
+    def extra_training_step(self, batch, batch_idx) -> Dict:
+        return {}
+
+    def extra_validation_step(self, batch, batch_idx) -> Dict:
+        return {}
